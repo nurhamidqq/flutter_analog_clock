cdc1c3496b63d4b845297d0db4b40595110bf92c
Nur Hamid <nurhamidqq@gmail.com>
2023-05-29 10:32:37 +0700

    Update second_handle_widget.dart

--- lib/second_handle_widget.dart
+++ lib/second_handle_widget.dart
@@ -86,13 +86,13 @@ class SecondHandleWidget extends StatelessWidget {
             controller
                 .animateTo(angle2, duration: const Duration(milliseconds: 100))
                 .whenComplete(() {
-              onDragHandleEnd(((controller.value / 360) * 3600).toInt());
+              onDragHandleEnd(((controller.value / 360) * 1).toInt());
             });
           } else {
             controller
                 .animateTo(angle1, duration: const Duration(milliseconds: 100))
                 .whenComplete(() {
-              onDragHandleEnd(((controller.value / 360) * 3600).toInt());
+              onDragHandleEnd(((controller.value / 360) * 1).toInt());
             });
           }
 
